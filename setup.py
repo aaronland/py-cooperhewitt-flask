33638f88dbae73b74551b4a992a65d448b91bbfc
straup <github@aaronland.net>
2014-11-13 07:42:01 -0500

    add a setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cooperhewitt-flask',
+      version='0.2',
+      description='Cooper Hewitt utility functions for Flask applications',
+      author='Cooper Hewitt Smithsonian Design Museum',
+      url='https://github.com/cooperhewitt/py-flask',
+      requires=[
+        'flask',
+        'flask-cors',
+        'werkzeug',
+        'werkzeug.security',
+        ],
+      packages=[
+          'cooperhewitt',
+          'cooperhewitt.flask'
+      ],
+      scripts=[],
+      download_url='https://github.com/cooperhewitt/py-cooperhewitt-flask/releases/tag/v0.2',
+      license='BSD')
